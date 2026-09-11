25ba3a22b913f6938c7f7d0572aed434dbf68186
sakeju <akejusamuel0@gmail.com>
2026-09-11 13:39:01 +0100

    feat:improved header

--- main.go
+++ main.go
@@ -4,32 +4,57 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from the other side"))
+
+	w.Header().Add("Server", "Go")
+	
+	w.Write([]byte("Hello from the other side\n"))
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet money..."))
+
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return 
+	}
+
+	msg := fmt.Sprintf("Display a specific snippet with with ID %d.....\n", id)
+	w.Write([]byte(msg))
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new snippet money..."))
+	w.Write([]byte("Display a form for creating a new snippet money...\n"))
+}
+// func exampleHandler(w http.ResponseWriter, r *http.Request) {
+	// category := r.PathValue("category")
+	// itemID := r.PathValue("itemID")
+// }
+
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+
+	w.WriteHeader(http.StatusCreated)
+
+	w.Write([]byte("Save a new snippet....\n"))
 }
 
 func main() {
 	
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on :9000")
+	log.Print("starting server on http://localhost:9000")
 
 
 	err := http.ListenAndServe(":9000", mux)
+	log.Print("This server was built by Akeju Samuel")
 	log.Fatal(err)
 
 	fmt.Println("Hello World!")
